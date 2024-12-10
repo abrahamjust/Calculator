def713a4edac79363a083e4d6565eac311dd46b7
Abraham Justin <162578329+abrahamjust@users.noreply.github.com>
2024-12-10 14:23:05 +0000

    added functionality for the numbers and operators

--- script.js
+++ script.js
@@ -1,41 +1,101 @@
 let operand1
 let operand2
 let operator
-
-function addition(a, b) {
-    console.log(a+b)
-}
-
-function subtraction(a, b) {
-    console.log(a-b)
-}
-
-function multiplication(a, b) {
-    console.log(a*b)
-}
-
-function division(a, b) {
-    console.log(a/b)
-}
+let displayNum = '0'
+let used = 0
 
 function operate(op1, op2, operator) {
+    let ans = 0
     switch(operator) {
         case '+':
-            addition(op1, op2)
+            ans = op1 + op2
+            operand1 = ans
+            displayNum = ans
+            setDisplay()
+            displayNum = '0'
             break
         case '-':
-            subtraction(op1, op2)
+            ans = op1 - op2
+            operand1 = ans
+            displayNum = ans
+            setDisplay()
+            displayNum = '0'
             break
         case '*':
-            multiplication(op1, op2)
+            ans = op1 * op2
+            operand1 = ans
+            displayNum = ans
+            setDisplay()
+            displayNum = '0'
             break
         case '/':
-            division(op1, op2)
+            ans = op1 / op2
+            operand1 = ans
+            displayNum = ans
+            setDisplay()
+            displayNum = '0'
             break
     }
 }
 
-operand1 = 1
-operand2 = 2
-operator = '+'
-operate(operand1, operand2, operator)
+function setDisplay() {
+    const disp = document.getElementById("display")
+    used = 1
+    if(displayNum.length > 9) {
+        let numDisp = displayNum.substring(0, 9)
+        disp.innerText = numDisp
+    } else {
+        disp.innerText = displayNum
+    }
+}
+
+function inputOperand(num) {
+    if(displayNum == '0') {
+        displayNum = num
+    } else if(displayNum.length < 9) {
+        displayNum = displayNum + num
+    }
+    setDisplay()
+}
+
+function Operator(op) {
+    if(operand1 == null) {
+        operand1 = Number(displayNum)
+        displayNum = ''
+        operator = op
+    } else {
+        operate(operand1, Number(displayNum), operator)
+        operator = op
+        console.log(operand1, " ", displayNum, " ", operator)
+    }
+}
+
+const number = document.getElementsByClassName("Numeric")
+for(let i = 0; i < number.length; i++) {
+    number[i].addEventListener("click", () => {
+        inputOperand(number[i].value)
+    })
+}
+
+const sign = document.getElementById("sign")
+sign.addEventListener("click", () => {
+    displayNum = (Number(displayNum) * -1).toString()
+    setDisplay()
+})
+
+const op = document.getElementsByClassName("Operator")
+for(let i = 0; i < op.length; i++) {
+    op[i].addEventListener("click", () => {
+        Operator(op[i].value)
+    })
+}
+
+const clear = document.getElementById("clear")
+clear.addEventListener("click", () => {
+    displayNum = '0'
+    operand1 = null
+    operand2 = null
+    operator = null
+    document.getElementById("display").innerText = ''
+    console.log("hello")
+})
